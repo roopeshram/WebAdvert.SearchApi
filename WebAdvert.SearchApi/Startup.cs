b256e04372bb83d7a316a112e4587ef7444b73e3
Roopesh Ramnivas <roopesh.ramnivas@gmail.com>
2019-12-07 00:29:26 -0600

    first push

---
+++ WebAdvert.SearchApi/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using WebAdvert.SearchApi.Extensions;
+using WebAdvert.SearchApi.Services;
+
+namespace WebAdvert.SearchApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddElasticSearch(Configuration);
+            services.AddTransient<ISearchService, SearchService>();
+
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            loggerFactory.AddAWSProvider(Configuration.GetAWSLoggingConfigSection(),
+                formatter:(loglevel,message, exception)=> $"[{DateTime.Now} {loglevel} {message} {exception?.Message} {exception?.StackTrace}");
+            app.UseMvc();
+        }
+    }
+}
